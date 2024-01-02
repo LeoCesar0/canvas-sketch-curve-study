8347c6f58506ed753e61a90f45a565cbb2111222
Leonardo César <leo_cesa@hotmail.com>
2024-01-02 10:58:40 -0300

    feat: add point on click outside any point

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const canvasSketch = require("canvas-sketch");
 const settings = {
   dimensions: [1080, 1080],
   animate: true,
-  
 };
 
 let canvasEl;
@@ -14,28 +13,66 @@ const sketch = ({ context, width, height, canvas }) => {
 
   points = [
     new Point({ x: width / 4, y: height / 2 }),
-    new Point({ x: 100, y: height / 4, control: true }),
+    new Point({ x: 100, y: height / 4 }),
     new Point({ x: width * 0.75, y: height / 2 }),
+    new Point({ x: width * 0.8, y: height * 0.25 }),
+    new Point({ x: width * 0.9, y: height * 0.4 }),
   ];
 
   canvas.addEventListener("mousedown", onMouseDown);
 
   return ({ context, width, height }) => {
+    // --------------------------
+    // Draw Canvas
+    // --------------------------
+
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
+
+    // --------------------------
+    // Draw point lines
+    // --------------------------
+
+    context.save();
+    context.strokeStyle = "#33333350";
+
+    context.beginPath();
+    context.moveTo(points[0].x, points[0].y);
+    points.forEach((point) => {
+      context.lineTo(point.x, point.y);
+    });
+    context.stroke();
+    context.restore();
+
+    // --------------------------
+    // Draw Curves
+    // --------------------------
 
     context.save();
     context.beginPath();
 
     context.moveTo(points[0].x, points[0].y);
 
-    context.quadraticCurveTo(
-      points[1].x,
-      points[1].y,
-      points[2].x,
-      points[2].y
-    );
+    for (let i = 0; i < points.length; i++) {
+      const curr = points[i];
+      const next = points[i + 1];
+
+      if (next) {
+        let mx = curr.x + (next.x - curr.x) / 2;
+        let my = curr.y + (next.y - curr.y) / 2;
+
+        context.quadraticCurveTo(curr.x, curr.y, mx, my);
+
+        if (i === points.length - 2) {
+          context.lineTo(next.x, next.y);
+        }
+      }
+    }
+
+    context.strokeStyle = "blue";
+    context.lineWidth = 4;
     context.stroke();
+
     context.restore();
 
     points.forEach((point) => point.draw(context));
@@ -55,18 +92,22 @@ const onMouseDown = (event) => {
     const hit = point.isHit(x, y);
     point.isDragging = hit;
   });
+
+  const hasHitPoint = points.find((point) => point.isDragging);
+  if (!hasHitPoint) {
+    points.push(new Point({ x, y }));
+  }
 };
 
 const onMouseMove = (event) => {
   const x = (event.offsetX / canvasEl.offsetWidth) * canvasEl.width;
   const y = (event.offsetY / canvasEl.offsetHeight) * canvasEl.height;
 
-  const draggingPoint = points.find((point) => point.isDragging)
+  const draggingPoint = points.find((point) => point.isDragging);
 
-  if(draggingPoint){
-    console.log('dragging')
-    draggingPoint.x = x
-    draggingPoint.y = y
+  if (draggingPoint) {
+    draggingPoint.x = x;
+    draggingPoint.y = y;
   }
 };
 
